e93512037b9e8f2e4e42dd2cf3108713de85008b
CLM <limin_chu@163.com>
2018-03-07 10:38:45 +0800

    webpack.config.js
    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,35 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-  // 入口
   entry: ['babel-polyfill','./index.js'],
-  output:{
-    filename:'index.js',
-    path:path.resolve(__dirname,'dist')
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
   },
   devServer: {
-    contentBase:'./dist',
-    hot:true,
-    compress:true,
-    port:9001,
-    clientLogLevel:'none',
-    quiet:true
+    contentBase: './dist',
+    hot: true,
+    compress: true,
+    port: 9000,
+    clientLogLevel: "none",
+    quiet: true
   },
   module: {
-    rules:[
+    rules: [
       {
-        test:/\.html$/,
-        use:'html-loader'
-      },
-      {
-        test:/\.js$/,
-        use:'babel-loader'
-      }
+        test: /\.html$/,
+        use: 'html-loader'
+      }, {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }]
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({template: './index.html'}),
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin()
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({template:'./index.html'}),
-    new webpack.HotModuleReplacementPlugin()
-  ]
-}
+  };
